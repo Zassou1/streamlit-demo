c1eccc4fd4c13b208fbc932039d88df071032d43
Zassou1 <lowestcouncil@gmail.com>
2026-07-08 15:05:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -105,10 +105,10 @@ level = st.slider("Choose a level", 1, 5)
 st.write(f"Your level: {get_level_description(level)}")
 
 # Text input
-name = st.text_input("Enter your name", "Type here...")
-if st.button("Submit"):
+name1 = st.text_input("Enter your name", "Type here...")
+if st.button("Submit1"):
     # Call the function with name and gender as arguments
-    st.success(generate_greeting(name, gender))
+    st.success(generate_greeting(name1, gender))
 
 
 st.subheader("Modification 3: String Methods")
